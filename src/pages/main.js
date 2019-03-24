8ac3cc8c1bd183cdfff203e59f4ec5f42a77f1bd
Vinicius Rufine <vsrufine@gmail.com>
2019-03-24 00:34:08 -0300

    listando produtos

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import api from './../services/api'
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity } from 'react-native'
 
 export default class Main extends Component {
   static navigationOptions = {
@@ -23,13 +23,24 @@ export default class Main extends Component {
     })
   }
 
+  renderItem = ({ item }) => (
+    <View>
+      <Text>{item.title}</Text>
+      <Text>{item.description}</Text>
+      <TouchableOpacity onPress={() => {}}>
+        <Text>Acessar</Text>
+      </TouchableOpacity>
+    </View>
+  )
+
   render() {
     return (
       <View>
-        <Text>Página Main</Text>
-        {
-          this.state.docs.map(product => <Text key={product._id}>- {product.title}</Text>)
-        }
+        <FlatList
+          data={this.state.docs}
+          keyExtractor={item => item._id}
+          renderItem={this.renderItem}
+        />
       </View>
     )
   }
