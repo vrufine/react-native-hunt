c581d41b8ea10355d7e1bfd3ed8e6b3e93fddc12
Vinicius Rufine <vsrufine@gmail.com>
2019-03-24 00:01:28 -0300

    estilizando header e status bar

--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,11 @@ import Main from './pages/main'
 
 export default createStackNavigator({
   Main
+}, {
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: '#da552f'
+    },
+    headerTintColor: '#fff'
+  }
 })
