017b0048e617a4440e6f888fb5e485a297e4fad2
Vinicius Rufine <vsrufine@gmail.com>
2019-03-24 01:14:15 -0300

    detalhe do produto com webview

--- src/pages/main.js
+++ src/pages/main.js
@@ -32,9 +32,14 @@ export default class Main extends Component {
     <View style={styles.productContainer}>
       <Text style={styles.productTitle}>{item.title}</Text>
       <Text style={styles.productDescription}>{item.description}</Text>
+
       <TouchableOpacity
         style={styles.productButton}
-        onPress={() => {}}
+        onPress={() => {
+          this.props.navigation.navigate('Product', {
+            product: item
+          })
+        }}
       >
         <Text style={styles.productButtonText}>Acessar</Text>
       </TouchableOpacity>
