dc73a5b2bcceda063354ca449e51350bc0e5892c
Vinicius Rufine <vsrufine@gmail.com>
2019-03-23 23:53:56 -0300

    configurando navegação

--- src/pages/main.js
+++ src/pages/main.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
 export default class Main extends Component {
+  static navigationOptions = {
+    title: 'JSHunt'
+  }
   render() {
     return (
       <View>
